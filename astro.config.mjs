1000e390ae65ec14508326506600ca102740e050
Luis Alatta <newluisalattago@gmail.com>
2026-04-10 04:00:06 -0500

    Fix: simplificando wrangler para evitar error de main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
-  // Definimos la salida como servidor para que el adaptador de Cloudflare funcione
-  output: 'server',
-  
+  output: 'server', // Esto es muy importante
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()]
   },
-
-  adapter: cloudflare({
-    // Esto asegura que use la configuración de tu archivo wrangler.jsonc
-    platformProxy: {
-      enabled: true,
-    },
-  })
+  adapter: cloudflare()
 });
