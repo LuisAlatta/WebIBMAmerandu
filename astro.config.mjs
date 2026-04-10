5a4b3fa03fc9074fb1898f223727d7bc1d1d6bd9
Luis Alatta <newluisalattago@gmail.com>
2026-04-10 03:53:59 -0500

    Fix: Cambiado output a server para Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,19 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  // Definimos la salida como servidor para que el adaptador de Cloudflare funcione
+  output: 'server',
+  
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
   },
 
-  adapter: cloudflare()
+  adapter: cloudflare({
+    // Esto asegura que use la configuración de tu archivo wrangler.jsonc
+    platformProxy: {
+      enabled: true,
+    },
+  })
 });
